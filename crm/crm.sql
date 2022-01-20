c4a9c21018d0ca02a40785be8bfd20d64c1d3f5d
giulzacc <giuliana.zaccardelli@kellogg.northwestern.edu>
2022-01-20 02:34:56 +0000

    building crm domain tables

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,42 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS salespersons;
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT, 
+  company_id INTEGER
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  salesperson_id INTEGER,
+  contact_id INTEGER,
+  type INTEGER,
+  date SMALLDATETIME,
+  notes TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT,
+  industry TEXT
+);
+
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
 
 -- CREATE TABLES
